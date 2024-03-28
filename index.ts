3bfb0949fc1feea61501f8713d5f7c4c43202270
Avneesh Sarwate <avneeshsarwate@gmail.com>
2024-03-28 01:13:59 -0400

    found osc library that properly works recieving messages from max

--- index.ts
+++ index.ts
@@ -1,21 +1,59 @@
-import { Client, Message, Server } from 'node-osc';
+// import { Client, Message, Server } from 'node-osc';
 
-const client = new Client('127.0.0.1', 5336);
-
-
+// const client = new Client('127.0.0.1', 5336);
 
 
-const oscServer = new Server(5335, '127.0.0.1');
 
-oscServer.on('message', function (msg) {
-  console.log("oscMessage", msg)
 
-  const outMsg = new Message('/newNotes');
+// const oscServer = new Server(5335, '127.0.0.1', () => {
+//   console.log("oscServer is running")
+// });
+
+// oscServer.on('message', function (msg) {
+//   console.log("oscMessage", msg)
+
+//   const outMsg = new Message('/newNotes');
   
-  client.send(outMsg, (err) => {
-    if (err) {
-      console.error(new Error(err.message));
-    }
-    client.close();
-  });
+//   client.send(outMsg, (err) => {
+//     if (err) {
+//       console.error(new Error(err.message));
+//     }
+//   });
+// });
+
+
+import { UDPPort } from 'osc';
+
+// Create an osc.js UDP Port listening on port 57121.
+var udpPort = new UDPPort({
+  localAddress: "127.0.0.1",
+  localPort: 5335,
+  metadata: true
 });
+
+// Listen for incoming OSC messages.
+udpPort.on("message", function (oscMsg, timeTag, info) {
+  console.log("An OSC message just arrived!", oscMsg);
+  console.log("Remote info is: ", info);
+});
+
+// Open the socket.
+udpPort.open();
+
+
+// When the port is read, send an OSC message to, say, SuperCollider
+// udpPort.on("ready", function () {
+//   udpPort.send({
+//       address: "/s_new",
+//       args: [
+//           {
+//               type: "s",
+//               value: "default"
+//           },
+//           {
+//               type: "i",
+//               value: 100
+//           }
+//       ]
+//   }, "127.0.0.1", 57110);
+// });
